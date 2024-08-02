21e298bec598faf7a764ca30b26eac29a1c70d16
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-08-02 14:34:06 +0200

    Add data to pagination

--- resources/js/Components/DataTable/DataTablePagination.tsx
+++ resources/js/Components/DataTable/DataTablePagination.tsx
@@ -1,12 +1,16 @@
 import { Pagination } from "@narsil-ui/Components";
+import { useDataTable } from "@narsil-table/Components";
 import * as React from "react";
 
 const DataTablePagination = React.forwardRef<HTMLDivElement, DataTablePaginationProps>(
 	({ collection, ...props }, ref) => {
+		const { tableStore } = useDataTable();
+
 		return (
 			<Pagination
 				ref={ref}
 				currentPage={collection.meta.currentPage}
+				data={tableStore.getParams()}
 				lastPage={collection.meta.lastPage}
 				links={collection.meta.links}
 				simpleLinks={collection.links}
