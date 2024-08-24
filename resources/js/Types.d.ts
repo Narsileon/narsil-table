39a572c350b527b2f2558dedd91984f27a6d9b0a
Narsilien <rigaux.jonathan@gmail.com>
2024-08-24 07:15:30 +0200

    Export types

--- resources/js/Types.d.ts
+++
@@ -1,7 +0,0 @@
-type DataTableCollection<T = { [key: string]: any }> = Collection<T> & {
-	columns: import("@tanstack/react-table").ColumnDef<any, any>[];
-};
-
-type ShowTableResource<T = { [key: string]: any }> = Resource<T> & {
-	columns: import("@tanstack/react-table").ColumnDef<any, any>[];
-};
