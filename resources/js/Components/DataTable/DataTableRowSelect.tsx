d894d07fb9dc9207675f0484e4319caa3b3263d9
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-08-28 12:02:35 +0200

    Add expanding button

--- resources/js/Components/DataTable/DataTableRowSelect.tsx
+++ resources/js/Components/DataTable/DataTableRowSelect.tsx
@@ -1,12 +1,27 @@
+import { ChevronDown } from "lucide-react";
 import { Row } from "@tanstack/react-table";
-import Checkbox, { CheckboxProps } from "@narsil-ui/Components/Checkbox/Checkbox";
+import Button, { ButtonProps } from "@narsil-ui/Components/Button/Button";
+import Checkbox from "@narsil-ui/Components/Checkbox/Checkbox";
 
-export interface DataTableRowSelectProps extends Partial<CheckboxProps> {
+export interface DataTableRowSelectProps extends Partial<ButtonProps> {
 	row: Row<any>;
 }
 
 const DataTableRowSelect = ({ row, ...props }: DataTableRowSelectProps) => {
-	return (
+	return row.getCanExpand() ? (
+		<Button
+			size='icon'
+			variant='ghost'
+			onClick={(event) => {
+				event.stopPropagation();
+
+				row.toggleExpanded();
+			}}
+			{...props}
+		>
+			<ChevronDown />
+		</Button>
+	) : (
 		<div className='flex items-center pl-2'>
 			<Checkbox
 				checked={row.getIsSelected()}
