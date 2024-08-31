b8f1d5ae1b0768180346148de5a731ff5791472b
Narsilien <rigaux.jonathan@gmail.com>
2024-08-31 15:35:25 +0200

    Fix resource update controller

--- src/Http/Controllers/ResourceUpdateController.php
+++ src/Http/Controllers/ResourceUpdateController.php
@@ -4,7 +4,6 @@ namespace Narsil\Tables\Http\Controllers;
 
 #region USE
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
