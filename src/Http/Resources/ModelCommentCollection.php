445825042d5fb5c79a42e4a7a4f28082228501b8
Narsilien <rigaux.jonathan@gmail.com>
2024-09-08 14:53:05 +0200

    Add collection

---
+++ src/Http/Resources/ModelCommentCollection.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Narsil\Tables\Http\Resources;
+
+#region USE
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use JsonSerializable;
+
+#endregion
+
+/**
+ * @version 1.0.0
+ *
+ * @author Jonathan Rigaux
+ */
+class ModelCommentCollection extends ResourceCollection
+{
+    #region PUBLIC METHODS
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonSerializable
+     */
+    public function toArray(Request $request): JsonSerializable
+    {
+        return $this->collection->map(function ($item)
+        {
+            return $item->toArray();
+        });
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function with($request): array
+    {
+        $columns = $this->getColumns()->map->get()->values();
+
+        return compact(
+            'columns',
+        );
+    }
+
+    #endregion
+}
