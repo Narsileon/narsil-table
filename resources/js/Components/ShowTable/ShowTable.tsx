c396830a74b5f232ec72df36fe136e18c54a0e2d
Narsilien <rigaux.jonathan@gmail.com>
2024-09-14 21:36:13 +0200

    Fix show table

--- resources/js/Components/ShowTable/ShowTable.tsx
+++ resources/js/Components/ShowTable/ShowTable.tsx
@@ -2,6 +2,7 @@ import { cn } from "@narsil-ui/Components";
 import { get, isNil } from "lodash";
 import { useTranslationsStore } from "@narsil-localization/Stores/translationStore";
 import * as React from "react";
+import ScrollArea from "@narsil-ui/Components/ScrollArea/ScrollArea";
 import ShowRow from "@narsil-tables/Components/ShowTable/ShowRow";
 import Table from "@narsil-ui/Components/Table/Table";
 import TableBody from "@narsil-ui/Components/Table/TableBody";
@@ -25,81 +26,86 @@ const ShowTable = React.forwardRef<HTMLTableElement, ShowTableProps>(
 		const { id = null, active = null, created_at = null, updated_at = null, ...attributes } = data;
 
 		return (
-			<Table
-				ref={ref}
-				className={cn("w-full min-w-full", className)}
-				{...props}
+			<ScrollArea
+				className='w-full rounded-md border'
+				orientation='horizontal'
 			>
-				{header ? (
-					<TableHeader>
-						<TableRow>
-							<TableHead>{trans("Attribut")}</TableHead>
-							<TableHead>{trans("Value")}</TableHead>
-						</TableRow>
-					</TableHeader>
-				) : null}
-				<TableBody>
-					{columns ? (
-						columns.map((column, index) => {
-							const value = get(data, column.accessorKey);
+				<Table
+					ref={ref}
+					className={cn("w-full min-w-full", className)}
+					{...props}
+				>
+					{header ? (
+						<TableHeader>
+							<TableRow>
+								<TableHead>{trans("Attribut")}</TableHead>
+								<TableHead>{trans("Value")}</TableHead>
+							</TableRow>
+						</TableHeader>
+					) : null}
+					<TableBody>
+						{columns ? (
+							columns.map((column, index) => {
+								const value = get(data, column.accessorKey);
 
-							return !(!nullable && isNil(value)) ? (
-								<ShowRow
-									attribute={column.header as string}
-									formatString={column?.meta?.formatString}
-									nullable={true}
-									type={column?.meta?.type ?? "string"}
-									value={value}
-									key={index}
-								/>
-							) : null;
-						})
-					) : (
-						<>
-							{id ? (
-								<ShowRow
-									attribute='id'
-									type='number'
-									value={id}
-								/>
-							) : null}
-							{active ? (
-								<ShowRow
-									attribute='active'
-									type='boolean'
-									value={active}
-								/>
-							) : null}
-							{Object.entries(attributes)?.map(([attribute, value], index) => {
-								return (
+								return !(!nullable && isNil(value)) ? (
 									<ShowRow
-										attribute={attribute}
-										type={"string"}
+										attribute={column.header as string}
+										formatString={column?.meta?.formatString}
+										nullable={true}
+										type={column?.meta?.type ?? "string"}
 										value={value}
 										key={index}
 									/>
-								);
-							})}
-							{created_at ? (
-								<ShowRow
-									attribute='created_at'
-									formatString='LLLL'
-									type='datetime-local'
-									value={created_at}
-								/>
-							) : null}
-							{updated_at ? (
-								<ShowRow
-									attribute='updated_at'
-									formatString='LLLL'
-									type='datetime-local'
-									value={updated_at}
-								/>
-							) : null}
-						</>
-					)}
-				</TableBody>
-			</Table>
+								) : null;
+							})
+						) : (
+							<>
+								{id ? (
+									<ShowRow
+										attribute='id'
+										type='number'
+										value={id}
+									/>
+								) : null}
+								{active ? (
+									<ShowRow
+										attribute='active'
+										type='boolean'
+										value={active}
+									/>
+								) : null}
+								{Object.entries(attributes)?.map(([attribute, value], index) => {
+									return (
+										<ShowRow
+											attribute={attribute}
+											type={"string"}
+											value={value}
+											key={index}
+										/>
+									);
+								})}
+								{created_at ? (
+									<ShowRow
+										attribute='created_at'
+										formatString='LLLL'
+										type='datetime-local'
+										value={created_at}
+									/>
+								) : null}
+								{updated_at ? (
+									<ShowRow
+										attribute='updated_at'
+										formatString='LLLL'
+										type='datetime-local'
+										value={updated_at}
+									/>
+								) : null}
+							</>
+						)}
+					</TableBody>
+				</Table>
+			</ScrollArea>
 		);
 	}
 );
